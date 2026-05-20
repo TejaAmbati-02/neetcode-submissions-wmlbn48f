82115163051d925b02861936e4979e33cefe7858
TejaSaiKumarAmbati <73156927+TejaAmbati-02@users.noreply.github.com>
2026-05-20 18:11:42 +0530

    Add: python-enumerate - submission-3

---
+++ Python Coding Interviews/python-enumerate/submission-3.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def get_index_of_seven(nums: List[int]) -> int:
+    found_of_7 = -1
+    for index,num in enumerate(nums):
+        if num == 7:
+            found_of_7 = index
+            break
+
+    return found_of_7
+
+
+
+def get_dist_between_sevens(nums: List[int]) -> int:
+    first_index_of_7 = -1
+    for index, value in enumerate(nums):
+        if value == 7:
+            if first_index_of_7 == -1:
+                first_index_of_7 = index
+            else:
+                return index - first_index_of_7
+    return -1
+
+
+
+# do not modify below this line
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(get_index_of_seven([1, 2, 3, 4, 5, 6, 8, 9]))
+print(get_index_of_seven([2, 4, 7, 5, 7, 8, 4, 2]))
+
+print(get_dist_between_sevens([1, 2, 7, 4, 5, 6, 7, 8, 9]))
+print(get_dist_between_sevens([2, 7, 7, 7, 8]))
+print(get_dist_between_sevens([7, 4, 8, 4, 2, 7]))
